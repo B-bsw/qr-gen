123e0ad62d10316a80c44c5946bdc6c75814c8fb
BB <sorawitthassadorn15@gmail.com>
2025-10-21 16:17:02 +0700

    update design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import QRCode from "qrcode";
 export default function Home() {
     const [text, setText] = useState<string | null>(null);
     const [qrImage, setQrImage] = useState<string | null>(null);
+
     useEffect(() => {
         if (!text) {
             setQrImage(null);
@@ -23,29 +24,29 @@ export default function Home() {
             .catch((err) => console.error(err));
     }, [text]);
     return (
-        <div className="flex justify-center items-center h-screen flex-col gap-5 mx-4 ">
-            {text && (
-                <div className="flex flex-col justify-center absolute top-10 items-center gap-8">
-                    {qrImage && (
-                        <div className="flex flex-col items-center gap-4">
-                            <img
-                                src={qrImage}
-                                alt="Generated QR"
-                                className="w-64 h-64 border-2 rounded-lg"
-                            />
-                        </div>
-                    )}
-                </div>
-            )}
-            <div>
-                <h1 className="text-3xl">QR Generator</h1>
+        <div className="flex justify-center items-center h-screen flex-col gap-5 mx-4">
+            <div className="w-64 h-64 border-2 rounded-lg border-stone-300/80 p-1">
+                {text && (
+                    <div>
+                        {qrImage && (
+                            <div className="flex flex-col items-center gap-4">
+                                <img src={qrImage} alt="QR Code" className="" />
+                            </div>
+                        )}
+                    </div>
+                )}
             </div>
-            <input
-                type="text"
-                className="border-2 rounded-md p-1 font-pormpt"
-                onChange={(e) => setText(e.target.value)}
-                placeholder="link url or text"
-            />
+            <div>
+                <h1 className="text-3xl">QR Code generate</h1>
+            </div>
+            <div className="w-64">
+                <input
+                    type="text"
+                    className="w-full p-1 border-2 border-zinc-300 rounded-md outline-0  font-pormpt "
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="https://"
+                />
+            </div>
         </div>
     );
 }
