199f75791d01305f6eb394fe380cae0cf299ddb6
B-bsw <sorawitthassadorn15@gmail.com>
2026-03-27 04:00:05 +0700

    fix:dependency and format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,33 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import QRCode from "qrcode";
+'use client'
+import React, { useState, useEffect } from 'react'
+import QRCode from 'qrcode'
 
 export default function Home() {
-    const [text, setText] = useState<string | null>(null);
-    const [qrImage, setQrImage] = useState<string | null>(null);
+    const [text, setText] = useState<string | null>(null)
+    const [qrImage, setQrImage] = useState<string | null>(null)
 
     useEffect(() => {
         if (!text) {
-            setQrImage(null);
-            return;
+            setQrImage(null)
+            return
         }
 
         QRCode.toDataURL(text, {
             width: 256,
             margin: 2,
             color: {
-                dark: "#000000",
-                light: "#ffffff",
+                dark: '#000000',
+                light: '#ffffff',
             },
         })
             .then((url) => setQrImage(url))
-            .catch((err) => console.error(err));
-    }, [text]);
+            .catch((err) => console.error(err))
+    }, [text])
     return (
-        <div className="flex justify-center items-center h-full flex-col gap-5 mx-4">
-            <div className="w-64 h-64 border-2 rounded-lg border-stone-300/80 p-1">
+        <div className="mx-4 flex h-full flex-col items-center justify-center gap-5">
+            <div
+                className={`h-64 w-64 rounded-lg border-2 border-stone-300/80 p-1`}
+            >
                 {text && (
                     <div>
                         {qrImage && (
@@ -42,11 +44,11 @@ export default function Home() {
             <div className="w-64">
                 <input
                     type="text"
-                    className="w-full p-1 border-2 border-zinc-300 rounded-md outline-0  font-pormpt "
+                    className="font-pormpt w-full rounded-md border-2 border-zinc-300 p-1 outline-0"
                     onChange={(e) => setText(e.target.value)}
-                    placeholder="https://"
+                    placeholder="https://example.com"
                 />
             </div>
         </div>
-    );
+    )
 }
